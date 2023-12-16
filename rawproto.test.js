d4826c37bc1ce588367c59987997187cc4713acc
David Konsumer <konsumer@jetboystudio.com>
2023-12-15 18:07:01 -0800

    update tests

--- rawproto.test.js
+++ rawproto.test.js
@@ -4,40 +4,55 @@ import rawprotoparse from './rawproto.js'
 
 const proto = await protobuf.load(new URL('test.proto', import.meta.url).pathname)
 
-const Test = proto.lookupType('Test')
 const Simple = proto.lookupType('Simple')
 
-describe('buffers made with protobufjs', () => {
-  test('Simple message', async () => {
-    const pb = Simple.encode({ a: 150 }).finish()
-    const r = await rawprotoparse(pb)
+describe('Test', () => {
+  const Test = proto.lookupType('Test')
+
+  const pb = Test.encode({
+    nums: [1, 2, 3, 4, 5],
+    num: 1,
+    str: 'hello',
+    children: [
+      { num: 1, str: 'cool', children: [{ num: 1 }] },
+      { num: 2, str: 'awesome', children: [{ num: 2 }] },
+      { num: 3, str: 'neat', children: [{ num: 3 }] }
+    ]
+  }).finish()
+
+  /*
+  0A 05 01 02  03 04 05 10
+  01 1A 05 68  65 6C 6C 6F
+  22 0C 08 01  12 04 63 6F
+  6F 6C 1A 02  08 01 22 0F
+  08 02 12 07  61 77 65 73
+  6F 6D 65 1A  02 08 02 22
+  0C 08 03 12  04 6E 65 61
+  74 1A 02 08  03
+  */
+
+  test('default: prefix=f, stringMode=auto, arrayMode=false', () => {
+    const r = rawprotoparse(pb)
     expect(r).toMatchSnapshot()
   })
 
-  test('Test message', async () => {
-    const pb = Test.encode({
-      nums: [1, 2, 3, 4, 5],
-      num: 1,
-      str: 'hello',
-      children: [
-        { num: 1, str: 'cool', children: [{ num: 1 }] },
-        { num: 2, str: 'awesome', children: [{ num: 2 }] },
-        { num: 3, str: 'neat', children: [{ num: 3 }] }
-      ]
-    }).finish()
+  test('prefix=a, stringMode=auto, arrayMode=false', () => {
+    const r = rawprotoparse(pb, 'a')
+    expect(r).toMatchSnapshot()
+  })
 
-    /*
-    0A 05 01 02  03 04 05 10
-    01 1A 05 68  65 6C 6C 6F
-    22 0C 08 01  12 04 63 6F
-    6F 6C 1A 02  08 01 22 0F
-    08 02 12 07  61 77 65 73
-    6F 6D 65 1A  02 08 02 22
-    0C 08 03 12  04 6E 65 61
-    74 1A 02 08  03
-    */
+  test('prefix=f, stringMode=auto, arrayMode=true', () => {
+    const r = rawprotoparse(pb, 'f', 'auto', true)
+    expect(r).toMatchSnapshot()
+  })
 
-    const r = rawprotoparse(pb)
+  test('prefix=f, stringMode=buffer, arrayMode=false', () => {
+    const r = rawprotoparse(pb, 'f', 'buffer')
+    expect(r).toMatchSnapshot()
+  })
+
+  test('prefix=f, stringMode=string, arrayMode=false', () => {
+    const r = rawprotoparse(pb, 'f', 'string')
     expect(r).toMatchSnapshot()
   })
 })
