3616200e753e63d8b884a90a49d69dd364b67503
David Konsumer <konsumer@jetboystudio.com>
2023-12-15 17:26:06 -0800

    use protobuf-codec

--- rawproto.js
+++ rawproto.js
@@ -1,117 +1,43 @@
-// read more here:
-// https://github.com/protobufjs/protobuf.js/wiki/How-to-reverse-engineer-a-buffer-by-hand
-// https://protobuf.dev/programming-guides/encoding/
+// TODO: inline this
 
-const decoder = new TextDecoder()
+import codec from 'protobuf-codec'
 
-// given a buffer, try to figure out how to return it
-export function autoString(bytes, prefix) {
-  if (bytes.byteLength === 0) {
-    return ''
-  }
-  
-  // try to parse it as a sub-message
-  try {
-    const v = rawprotoparse(bytes, prefix)
-    return v
-  } catch(e){}
+const { decode: { reader, types, wireTypes: { decoders } } } = codec
 
-  // search buffer for extended chars
-  let hasExtended = false
-
-  for (const b of bytes) {
-    if (b < 32) {
-      hasExtended = true
-    }
-  }
-
-  return hasExtended ? bytes : decoder.decode(bytes)
-}
-
-export function rawprotoparse (buffer, prefix = 'f', lengthDeliminted=false, stringMode='auto') {
-  if (!(buffer instanceof Uint8Array)) {
-    throw new Error('bytes parameter should be a Uint8Array or Buffer.')
-  }
-
-  let bytes = buffer
-  let dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength)
-  let p = 0
-
-  if (lengthDeliminted) {
-    const len = getVal(0)
-    bytes = bytes.slice(0, len)
-    dv = new DataView(bytes.buffer, bytes.byteOffset, bytes.byteLength)
-    p = 0
-  }
-
+// entry-point util function that will assemble the protobuf into a js-object
+export function rawprotoparse (buffer, prefix = 'f', lengthDeliminted = false, stringMode = 'auto') {
   const out = {}
+  for (const [fieldNumber, { data, wireType }] of reader(buffer)) {
+    out[`${prefix}${fieldNumber}`] ||= []
 
-  function getVal (wireType) {
-    let len
-    let i
-    let value
-    switch (wireType) {
-      case 0:
-        i = 0
-        value = bytes[p]
-        if (!(bytes[p] & 0x80)) {
-          p++
-          return value
-        }
-        while (bytes[p] & 0x80) {
-          value |= (bytes[p++] & 0x7f) << i++ * 7
-        }
-        return value
-      case 1:
-        value = dv.getFloat64(p, true)
-        p += 8
-        return value
-      case 2:
-        len = getVal(0)
-        value = bytes.slice(p, p + len)
-        p += len
+    // console.log(fieldNumber, wireType, data)
+    if (wireType === 0) {
+      out[`${prefix}${fieldNumber}`].push(parseInt(data)) // could be bad for big numbers
+    }
+
+    if (wireType === 1) {
+      out[`${prefix}${fieldNumber}`].push(data)
+    }
+
+    if (wireType === 2) {
+      try {
+        out[`${prefix}${fieldNumber}`].push(rawprotoparse(data, prefix))
+      } catch (e) {
         if (stringMode === 'auto') {
-          return autoString(value, prefix)
+          const lowBytes = false
+          if (data.find(b => b < 32)) {
+            out[`${prefix}${fieldNumber}`].push(data)
+          } else {
+            out[`${prefix}${fieldNumber}`].push(types.string(data))
+          }
+        } else if (stringMode === 'string') {
+          out[`${prefix}${fieldNumber}`].push(types.string(data))
+        } else {
+          out[`${prefix}${fieldNumber}`].push(data)
         }
-        if (stringMode === 'string') {
-          return decoder.decode(value)
-        }
-        // binary
-        return value
-      case 3:
-        p++
-        return undefined
-      case 4:
-        p++
-        return undefined
-      case 5:
-        value = dv.getFloat32(p, true)
-        p += 8
-        return value
-      default:
-        p++
-        throw new Error(`Invalid wireType: ${wireType}`)
-    }
-  }
-
-  while (p < bytes.byteLength) {
-    const tag = getVal(0)
-    const id = tag >>> 3
-    const wireType = tag & 7
-
-    if (id === 0) {
-      throw new Error('0 is invalid field-id')
-    }
-
-    if ([0, 1, 2, 5].includes(wireType)) {
-      const v = getVal(wireType)
-      if (typeof v !== 'undefined') {
-        out[`${prefix}${id}`] ||= []
-        out[`${prefix}${id}`].push(v)
       }
     }
   }
-
   return out
 }
 
--- rawproto.test.js
+++ rawproto.test.js
@@ -7,28 +7,14 @@ const proto = await protobuf.load(new URL('test.proto', import.meta.url).pathnam
 const Test = proto.lookupType('Test')
 const Simple = proto.lookupType('Simple')
 
-describe('documented hand-parsing', () => {
-  test('message from protobufjs wiki', () => {
-    const pb = Buffer.from([0X0A, 0X0A, 0X32, 0X08, 0X08, 0X07, 0X10, 0X00, 0X18, 0X1A, 0X20, 0X00, 0X0A, 0X08, 0X22, 0X06, 0X08, 0X02, 0X10, 0X19, 0X18, 0X03, 0X0A, 0X09, 0X22, 0X07, 0X08, 0X02, 0X10, 0XA2, 0X03, 0X18, 0X20, 0X0A, 0X09, 0X22, 0X07, 0X08, 0X02, 0X10, 0X8D, 0X02])
-    const r = rawprotoparse(pb)
-    expect(r).toMatchSnapshot()
-  })
-
-  test('protobufjs#143', () => {
-    const pb = Buffer.from([0x0A, 0x0F, 0x72, 0x62, 0x68, 0x5F, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x5F, 0x76, 0x30, 0x2E, 0x31, 0x10, 0x06, 0x42, 0x1E, 0x0A, 0x16, 0x6C, 0x69, 0x76, 0x69, 0x6E, 0x67, 0x72, 0x6F, 0x6D, 0x5F, 0x73, 0x65, 0x6E, 0x73, 0x6F, 0x72, 0x6D, 0x6F, 0x64, 0x75, 0x6C, 0x65, 0x12, 0x04, 0x74, 0x65, 0x6D, 0x70])
-    const r = rawprotoparse(pb)
-    console.log(r)
-    expect(r).toMatchSnapshot()
-  })
-})
-
 describe('buffers made with protobufjs', () => {
-  test.skip('Simple message', () => {
+  test('Simple message', async () => {
     const pb = Simple.encode({ a: 150 }).finish()
-    const r = rawprotoparse(pb)
+    const r = await rawprotoparse(pb)
+    expect(r).toMatchSnapshot()
   })
 
-  test('Test message', () => {
+  test('Test message', async () => {
     const pb = Test.encode({
       nums: [1, 2, 3, 4, 5],
       num: 1,
